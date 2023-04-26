fb200dbe7ff08f2010f988e56a5fb379b6071eab
Paco8 <5084042+Paco8@users.noreply.github.com>
2023-04-26 12:53:40 +0200

    Update to 0.9.8~beta

--- plugin.video.amazon-test/resources/lib/playback.py
+++ plugin.video.amazon-test/resources/lib/playback.py
@@ -322,19 +322,22 @@ def PlayVideo(name, asin, adultstr, streamtype, forcefb=0):
         # and try again. This is neccessary for content like Amazon Freevee, which is not
         # available though token based authentification.
         
-        for preferTokenToCookie in ([True, False] if g.platform & g.OS_ANDROID else [False]):
+        for preferTokenToCookie in ([True, False]):
             cookie, opt_lic, headers, dtid = _getPlaybackVars(preferToken=preferTokenToCookie)
             if not cookie:
                 g.dialog.notification(getString(30203), getString(30200), xbmcgui.NOTIFICATION_ERROR)
                 Log('Login error at playback')
+                return False
+            '''
             if (g.platform & g.OS_ANDROID) and not isinstance(cookie, dict) and getConfig('uhdinfo') == '':
                 g.dialog.ok(g.__plugin__, getString(30272))
                 writeConfig('uhdinfo', '1')
+            '''
 
             success, data = getURLData('catalog/GetPlaybackResources', asin, extra=True, vMT=vMT, dRes=dRes, useCookie=cookie, devicetypeid=dtid,
                                        proxyEndpoint=(None if bypassproxy else 'gpr'), opt=opt)
 
-            if success:
+            if success or not isinstance(cookie, dict):
                 break
 
         mpd, subs, timecodes = _ParseStreams(success, data, retmpd=True, bypassproxy=bypassproxy)
@@ -446,7 +449,7 @@ def PlayVideo(name, asin, adultstr, streamtype, forcefb=0):
         return False
 
     isAdult = adultstr == '1'
-    amazonUrl = g.BaseUrl + "/dp/" + (name if g.UsePrimeVideo else asin)
+    amazonUrl = g.BaseUrl + "/dp/" + (py2_decode(name) if g.UsePrimeVideo else asin)
     videoUrl = "%s/?autoplay=%s" % (amazonUrl, ('trailer' if streamtype == 1 else '1'))
     extern = not xbmc.getInfoLabel('Container.PluginName').startswith('plugin.video.amazon')
     suc = False
